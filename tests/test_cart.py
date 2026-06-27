46db0abfcab21c26bd9c320d03022c6994e8a2ab
Patrick Radke <47391200+PatrickRadke@users.noreply.github.com>
2026-06-27 18:56:24 +0200

    Aufgabe 2: 🔴 Test Hinzufügen eines einzelnen Artikels zum Warenkorb

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,1 +1,12 @@
+import pytest
+from cart.cart import ShoppingCart
+
+@pytest.fixture
+def cart():
+    return ShoppingCart()
+
+def test_add_item(cart):
+    cart.add_item('apple', 0.50)
+    assert cart.item_count == 1
+
 
